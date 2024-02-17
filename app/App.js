bcdb2ed0e888c38af6f4e8e02a46b07d6d0e1f4e
Blake Lawatch <blakelawatch@gmail.com>
2024-02-16 20:43:27 -0700

    fixed issues

--- app/App.js
+++ app/App.js
@@ -1,14 +1,27 @@
+import Aos from 'aos';
 import { router } from './router-config.js';
-import AOS from 'aos'
+
 
 class App {
 
   router = router
 
+
 }
+// function aos_init() {
+//   Aos.init({
+//     duration: 1000,
+//     once: true
+//   });
+// }
+// aos_init();
+// $(window).on('load', function() {
+// });
 
 
 const app = new App()
 // @ts-ignore
 window.app = app
+
+
 
--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,9 +1,14 @@
 import { Pop } from "../utils/Pop.js"
+import Aos from "aos";
 
 // Public
 export class HomeController {
   constructor() {
     console.log('This is the Home Controller')
+    Aos.init({
+      once: true,
+      duration: 1000
+    });
   }
 
   testButton() {
